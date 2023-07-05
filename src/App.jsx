325046bea07084c9abd52ce9a2ab7953e03792fc
Andrew Rivero <andyyR134@gmail.com>
2023-07-05 00:16:35 -0700

    Updated Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import { BrowserRouter } from "react-router-dom";
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from './components';
+
 const App = () => {
   return (
-    <div>
-      3D Dev Portfolio
-    </div>
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        
+      </div>
+    </BrowserRouter>
   )
 }
 
